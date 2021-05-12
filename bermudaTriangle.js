a8c42b96b214e355216a31c1f91d0e28280fccfc
Ankur598 <ankursingh.as278@gmail.com>
2021-05-12 22:18:23 +0530

    done

--- bermudaTriangle.js
+++ bermudaTriangle.js
@@ -9,3 +9,13 @@ for (var i = 1; i <= 6; i++) {
     }
     console.log(res);
 }
+var a = 10;
+  var b =10 ;
+  var p = a.length;
+  var q = b.length;
+  for(var i = 0; i<p-q+1;i++){
+    console.log(i);
+  for(var j = 0; j<q;j++){
+  console.log("*");
+  }
+  }
