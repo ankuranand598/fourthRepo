fb61fe32b63cbde9a754935d1bb9ad43c0101306
Ankur598 <ankursingh.as278@gmail.com>
2021-05-14 20:51:19 +0530

    string match

---
+++ stringMatch.js
@@ -0,0 +1,23 @@
+var a = "ab123b";
+var num = "1234567890";
+num.split("");
+var count = 0;
+if (typeof a == "string") {
+    var t = a.split("");
+}
+console.log(t);
+/*for (var i = 1; i <= t.length; i++) {
+    for (var j = 0; j < num.length; i++){
+        if (t[i] == num[j]) {
+            count++;
+        }
+    }
+}
+if (t.length == count) {
+    console.log("numbers");
+} else if (t.length > count && count != 0) {
+    console.log("Alphanumeric");
+
+} else {
+    console.log("Char");
+}*/
