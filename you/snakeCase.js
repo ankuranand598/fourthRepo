d512b14d88811f73a639e80c533edfcad789013d
Ankur598 <ankursingh.as278@gmail.com>
2021-05-19 17:47:04 +0530

    snake case

---
+++ you/snakeCase.js
@@ -0,0 +1,5 @@
+//Given an array of strings print the snake case of the strings [Assume Small case strings],
+var arr = ["apple", "windows", "ubuntu"];//input,
+//join is a inbuilt function which takes array and returns string.
+var op = arr.join("_");
+console.log(op);//apple_windows_ubuntu - is op
